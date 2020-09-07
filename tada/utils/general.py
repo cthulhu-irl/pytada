e66793a1d4c9df6134cd68ebdf8299d77068c0e7
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 06:52:06 +0430

    feat: implement prop, apply_if_else, apply_if functions

--- tada/utils/general.py
+++ tada/utils/general.py
@@ -1,16 +1,16 @@
-from .functional import curry
+from .functional import identity, curry
 
 @curry(2)
 def prop(attr, obj):
-    pass
+    return getattr(attr, obj)
 
 @curry(4)
 def apply_if_else(constraint, if_fn, else_fn, obj):
-    pass
+    return if_fn(obj) if constraint(obj) else else_fn(obj)
 
 @curry(3)
 def apply_if(constraint, fn, obj):
-    pass
+    return apply_if_else(constraint, fn, identity, obj)
 
 def fix_status(todo):
     pass
