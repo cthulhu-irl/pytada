81e30c4e7c1dfdf2e4b3d198263ab33bc818be03
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 07:41:09 +0430

    feat: implement Status class

--- tada/models/status.py
+++ tada/models/status.py
@@ -10,20 +10,32 @@ class StatusBase(ABC, Comparable, Model):
         pass
 
 class Status(StatusBase):
+    STRINGS = ['-', 'x', '+']
 
-    def __init__(self, string):
-        pass
+    def __init__(self, stat):
+        if isinstance(stat, str) and stat in self.STRINGS:
+            self.string = stat
+            self.index = self.STRINGS.index(stat)
+
+        strs_len = len(self.STRINGS)
+        if isinstance(stat, int) and 0 <= stat < strs_len:
+            self.string = self.STRINGS[stat]
+            self.index = stat
+
+        raise ValueError(
+            'given `stat` is not an Status representative'
+        )
 
     def __str__(self):
-        pass
+        return self.string
 
     def __cmp__(self, other):
-        pass
+        return self.index.__cmp__(other.index)
 
     def to_raw(obj):
-        pass
+        return str(obj)
 
     @classmethod
     def from_raw(cls, string):
-        pass
+        return cls(string)
 
