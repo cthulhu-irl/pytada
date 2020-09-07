72cb0538f1eced0ea23f07363d31305c45b1bd2b
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 16:29:36 +0430

    feat: implement Query class

--- tada/query/query.py
+++ tada/query/query.py
@@ -1,48 +1,52 @@
 from ..utils.funcional import identity
+from .constraints import and_fn, or_fn, xor_fn
 
 class Query(object):
 
     def __init__(self, selector=identity):
-        pass
+        self.fn = selector
 
     #-- syntactic sugar
 
     def __call__(self, obj):
-        pass
+        return self.query(obj)
 
-    def __lshift__(self, query):
-        pass
+    def __lshift__(self, selector):
+        return self.then(query)
 
-    def __rshift__(self, selector):
-        pass
+    def __and__(self, selector):
+        return self.and_(selector)
 
-    def __or__(self, obj):
-        pass
+    def __or__(self, selector):
+        return self.or_(selector)
+
+    def __xor__(self, selector):
+        return self.xor_(selector)
 
     #-- functor
 
     def map(self, fn):
-        pass
+        return Query(fn(self.fn))
 
     def join(self):
-        pass
+        return self.fn
 
     #-- functionality
 
     def then(self, selector):
-        pass
+        return self.map(compose(selector))
 
     def query(self, obj):
-        pass
+        return self.fn(obj)
 
     #-- helpers
 
-    def and(self, selector):
-        pass
+    def and_(self, selector):
+        return self.map(lambda fn: and_fn(fn, selector))
 
-    def or(self, selector):
-        pass
+    def or_(self, selector):
+        return self.map(lambda fn: or_fn(fn, selector))
 
-    def xor(self, selector):
-        pass
+    def xor_(self, selector):
+        return self.map(lambda fn: xor_fn(fn, selector))
 
