287c933ea7880800e76ee7eaac63702c6a61148a
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 06:12:42 +0430

    skeleton: add apply_if_else and prop functions

--- tada/utils/general.py
+++ tada/utils/general.py
@@ -1,5 +1,11 @@
-def fix_status(todo):
+def prop(attr, obj):
+    pass
+
+def apply_if_else(constraint, if_fn, else_fn, obj):
     pass
 
 def apply_if(constraint, fn, obj):
+    pass
+
+def fix_status(todo):
     pass
