1e53842c36ad4cdec7edf93be6eed9a2288ad13b
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 06:49:41 +0430

    feat: add curry decorator to general utility functions

--- tada/utils/general.py
+++ tada/utils/general.py
@@ -1,9 +1,14 @@
+from .functional import curry
+
+@curry(2)
 def prop(attr, obj):
     pass
 
+@curry(4)
 def apply_if_else(constraint, if_fn, else_fn, obj):
     pass
 
+@curry(3)
 def apply_if(constraint, fn, obj):
     pass
 
