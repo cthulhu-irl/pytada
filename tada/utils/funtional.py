2fd9b122c62673fc95b593ab45f0a479835cb527
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 06:38:42 +0430

    feat: implement curry decorator function

--- tada/utils/funtional.py
+++ tada/utils/funtional.py
@@ -1,8 +1,21 @@
 def identity(x):
     return x
 
-def curry(f):
-    pass
+def curry(arg_count):
+    fn_args = []
+    fn_kwargs = {}
+
+    def _decorator(fn):
+        def _currier(*args, **kwargs):
+            fn_args.extend(args)
+            fn_kwargs.update(kwargs)
+
+            if len(fn_args) >= arg_count:
+                return fn(*fn_args, **fn_kwargs)
+
+        return _currier
+
+    return _decorator
 
 def compose(f, g, *fns):
     pass
