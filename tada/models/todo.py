226973382786437b032dec4a9aab36851d1218da
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-07 13:19:23 +0430

    feat: implement Todo class

--- tada/models/todo.py
+++ tada/models/todo.py
@@ -1,6 +1,13 @@
 from .base import Model
+from .status import Status
 
 class Todo(Model):
+
+    class Repr:
+        STATUS  = 'status'
+        TITLE   = 'title'
+        INFO    = 'info'
+        SUBLIST = 'sublist'
 
     def __init__(self, status, title, info={}, sublist=[]):
         self.status = status
@@ -9,12 +16,25 @@ class Todo(Model):
         self.sublist = sublist
 
     def __str__(self):
-        pass
+        return '{} {}'.format(self.status, self.title)
 
     def to_raw(obj):
-        pass
+        return {
+            obj.Repr.STATUS: str(obj.status),
+            obj.Repr.TITLE: str(obj.title),
+            obj.Repr.INFO: obj.info.copy(),
+            obj.Repr.SUBLIST: [
+                child.to_raw() for child in obj.sublist
+            ]
+        }
 
     @classmethod
     def from_raw(cls, raw):
-        pass
+        sublist = raw[cls.Repr.SUBLIST]
+        return cls(
+            Status(raw[cls.Repr.STATUS]),
+            str(raw[cls.Repr.TITLE]),
+            raw[obj.Repr.INFO],
+            [cls.from_raw(child) for child in sublist]
+        )
 
