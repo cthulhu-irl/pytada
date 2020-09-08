c7d4cdd6f0ad03126a304c79ba8756c1cbde5c3a
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-08 14:01:07 +0430

    doc: add documentation for Constraint class and its methods

--- tada/query/constraints.py
+++ tada/query/constraints.py
@@ -1,6 +1,9 @@
 from ..utils.general import and_fn, or_fn, xor_fn
 
 class Constraint(object):
+    '''
+    logically composable contraint wrapper by infix operators.
+    '''
 
     def __init__(self, fn):
         self.fn = fn
@@ -30,16 +33,34 @@ class Constraint(object):
     #-- functionality
 
     def match(self, *args, **kwargs):
+        '''
+        calls beneath constraint function and returns result.
+        '''
         return self.fn(*args, **kwargs)
 
     #-- logical combinations
 
     def and_(self, other):
+        '''
+        compose with another constraint by and logical function.
+        it is same as and_fn(self, other) but returning
+        a constraint instance.
+        '''
         return self.map(lambda f: and_fn(f, other))
 
     def or_(self, other):
+        '''
+        compose with another constraint by or logical function.
+        it is same as or_fn(self, other) but returning
+        a constraint instance.
+        '''
         return self.map(lambda f: or_fn(f, other))
 
     def xor_(self, other):
+        '''
+        compose with another constraint by xor logical function.
+        it is same as xor_fn(self, other) but returning
+        a constraint instance.
+        '''
         return self.map(lambda f: xor_fn(f, other))
 
