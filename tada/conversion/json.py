211285378b31ed96e4bc238aced358811f527004
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-10 14:21:10 +0430

    lint(conversion): add documentation and conform to flake8 rules

--- tada/conversion/json.py
+++ tada/conversion/json.py
@@ -1,12 +1,16 @@
 from abc import ABC, abstractmethod
 
+
 class JSONConvertable(ABC):
 
     @abstractmethod
     def to_json(obj):
+        """ convert the given obj to json string """
         pass
 
     @abstractmethod
     def from_json(cls, string):
+        """
+        converts the given json string to an instance of this class
+        """
         pass
-
--- tada/conversion/raw.py
+++ tada/conversion/raw.py
@@ -1,12 +1,19 @@
 from abc import ABC, abstractmethod
 
+
 class RAWConvertable(ABC):
 
     @abstractmethod
     def to_raw(obj):
+        """
+        converts the given obj to pure primary types instances
+        """
         pass
 
     @abstractmethod
-    def from_raw(cls, string):
+    def from_raw(cls, raw):
+        """
+        converts the given raw of pure primary types to
+        an instance of this class
+        """
         pass
-
