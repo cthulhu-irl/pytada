051067985c99714b71a79e4bc70c56aed8d2fb6b
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-10 13:44:15 +0430

    lint: add documentation and conform to flake8 rules

--- tada/utils/funtional.py
+++ tada/utils/funtional.py
@@ -1,7 +1,13 @@
+""" functional programming utilities """
+
+
 def identity(x):
+    """ always return what's given """
     return x
 
+
 def curry(arg_count):
+    """ function decorator for currying function's arguments """
     fn_args = []
     fn_kwargs = {}
 
@@ -17,15 +23,31 @@ def curry(arg_count):
 
     return _decorator
 
+
 @curry(2)
 def compose(f, g, *fns):
+    """
+    composes given functions together as when the returned
+    function called, the given arguments would go to last
+    given function, and then the result of last given
+    function is passed as argument to the given function
+    before last one... and this goes up until the first,
+    then the result is returned.
+    """
+
     def _composer(*args, **kwargs):
         if fns:
             first, *rest = fns
-            g = compose(g, first, *rest)
+            return f(compose(g, first, *rest)(*args, **kwargs))
 
         return f(g(*args, **kwargs))
 
+    return _composer
+
+
 @curry(2)
 def fmap(fn, obj):
+    """
+    maps the given obj to given fn through obj's map method
+    """
     return obj.map(fn)
