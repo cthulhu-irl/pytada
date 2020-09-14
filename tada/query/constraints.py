e70a49de92e40c09ba9fab623bc7c5a05921614a
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-15 00:29:40 +0430

    feat: make all query.constraints logically composable by adding Constraint to them as decorator

--- tada/query/constraints.py
+++ tada/query/constraints.py
@@ -1,20 +1,24 @@
 import re
 
+from .constraint import Constraint
 from ..utils.functional import curry
 
 
 # -- string/array related constraints
 
+@Constraint
 @curry(2)
 def contains(element, lst):
     return element in lst
 
 
+@Constraint
 @curry(2)
 def intersects(lst1, lst2):
     return all(elem in lst2 for elem in lst1)
 
 
+@Constraint
 @curry(2)
 def starts_with(sub, lst):
     if len(sub) > len(lst):
@@ -27,11 +31,13 @@ def starts_with(sub, lst):
     return True
 
 
+@Constraint
 @curry(2)
 def ends_with(sub, lst):
     return starts_with(sub[::-1], lst[::-1])
 
 
+@Constraint
 @curry(2)
 def regex_match(regex, string):
     return bool(re.match(regex, string))
@@ -39,36 +45,43 @@ def regex_match(regex, string):
 
 # -- comparison constraints
 
+@Constraint
 @curry(2)
 def inrange(start, end, num):
     return start <= num <= end
 
 
+@Constraint
 @curry(2)
 def eq(a, b):
     return a == b
 
 
+@Constraint
 @curry(2)
 def neq(a, b):
     return a != b
 
 
+@Constraint
 @curry(2)
 def lt(a, b):
     return a < b
 
 
+@Constraint
 @curry(2)
 def lte(a, b):
     return a <= b
 
 
+@Constraint
 @curry(2)
 def gt(a, b):
     return a > b
 
 
+@Constraint
 @curry(2)
 def gte(a, b):
     return a >= b
