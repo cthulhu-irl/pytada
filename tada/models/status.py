230a1579d3603cb55d308f18b59ebf2c465e70b8
cthulhu-irl <xh4x0r3r@gmail.com>
2020-09-14 17:35:07 +0430

    fix: use if-elif-else to avoid falloff in Status.__init__

--- tada/models/status.py
+++ tada/models/status.py
@@ -25,18 +25,20 @@ class Status(StatusBase):
     STRINGS = ['-', 'x', '+']
 
     def __init__(self, stat):
+        strs_len = len(self.STRINGS)
+
         if isinstance(stat, str) and stat in self.STRINGS:
             self.string = stat
             self.index = self.STRINGS.index(stat)
 
-        strs_len = len(self.STRINGS)
-        if isinstance(stat, int) and 0 <= stat < strs_len:
+        elif isinstance(stat, int) and 0 <= stat < strs_len:
             self.string = self.STRINGS[stat]
             self.index = stat
 
-        raise ValueError(
-            'given `stat` is not an Status representative'
-        )
+        else:
+            raise ValueError(
+                'given `stat` is not an Status representative'
+            )
 
     def __str__(self):
         return self.string
