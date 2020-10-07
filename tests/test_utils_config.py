478c427ad983ef96b7e0ae2fa280d64213d00b5b
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-07 12:16:52 +0330

    refactor: rewrite tests for tada.utils.config

--- tests/test_utils_config.py
+++ tests/test_utils_config.py
@@ -12,14 +12,10 @@ class DataBaseEnum(str, enum.Enum):
     POSTGRESQL = 'postgres'
 
 class DefaultSection(config.Section):
-    NAME: ClassVar[str] = 'general'
-
     name: str
     db: DataBaseEnum = Field(default=DataBaseEnum.SQLITE, title='database')
 
 class DataBaseSection(config.Section):
-    NAME: ClassVar[str] = 'database'
-
     host: str
     port: int = Field(default=8021, gt=0, lt=65535, typeconv=int)
     username: str = 'admin'
@@ -27,51 +23,16 @@ class DataBaseSection(config.Section):
 
 class AppConfig(config.Config):
     default: DefaultSection
-    database: DataBaseSection = Field(default_factory=DataBaseSection)
+    database: DataBaseSection
 
-def test_section_field_schema_mapping() -> None:
-    schema = DefaultSection.section_schema()
+def test_instantiation() -> None:
+    default_section = DefaultSection(name='test')
+    db_section = DataBaseSection(host='localhost')
 
-    assert list(schema.fields.keys()) == ['name', 'db']
+    with pytest.raises(Exception):
+        AppConfig(default=default_section)
 
-    assert schema.name.required
-    assert schema.name.name == 'name'
-    assert schema.name.typeclass == str
-    assert schema.name.typeconv is None
-    assert schema.name.help == ''
-    assert schema.name.default is None
+    config = AppConfig(default=default_section, database=db_section)
 
-    assert schema.db.name == 'database'
-    assert schema.db.default == DataBaseEnum.SQLITE
-
-    schema = DataBaseSection.section_schema()
-
-    assert schema.host.required
-    assert schema.port.typeconv == int
-    assert schema.port.default == 8021
-    assert schema.username.default == 'admin'
-    assert schema.password.default == 'admin'
-
-def test_config_fields_section_type_enforce_with_default() -> None:
-    general_section = DefaultSection(name='test field name')
-
-    with pytest.raises(Exception) as excinfo:
-        class IllConfig(config.Config):
-            general: DefaultSection
-            something: str = 'some default to evade init enforce'
-
-        IllConfig(general=general_section)
-
-    assert excinfo
-
-def test_config_fields_section_type_enforce() -> None:
-    general_section = DefaultSection(name='test field name')
-
-    with pytest.raises(Exception) as excinfo:
-        class IllConfig(config.Config):
-            general: DefaultSection
-            something: str
-
-        IllConfig(general=general_section, something='test')
-
-    assert excinfo
+    assert config.default.name == 'test'
+    assert config.database.host == 'localhost'
