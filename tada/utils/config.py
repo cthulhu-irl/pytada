bc9cde35aa6adf08f1a3e6d3550d07128427fdf0
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-07 12:03:54 +0330

    refactor: no FieldSchema and schema related stuff and enforcements

--- tada/utils/config.py
+++ tada/utils/config.py
@@ -1,59 +1,6 @@
-from inspect import getmro
-from typing import (
-    Optional, Any, Dict, Tuple, Type, Callable, ClassVar, List
-)
+from typing import ClassVar
 
-from pydantic import BaseModel, ValidationError, validator
-from pydantic.fields import ModelField
-
-from .structures import restrict_field_types
-
-class FieldSchema(BaseModel):
-    FIELD_SCHEMA: ClassVar[str] = 'field_schema'
-    TYPECONV: ClassVar[str] = 'typeconv'
-
-    required: bool
-    name: str
-    typeclass: Any
-    typeconv: Optional[Callable[[Any], Any]] = None
-    help: Optional[str] = ''
-    default: Optional[Any] = None
-
-class SectionSchema(object):
-
-    def __init__(
-        self,
-        fieldschema_factory=FieldSchema,
-        **fields: FieldSchema
-    ) -> None:
-        self.fieldschema_factory = fieldschema_factory
-        self.fields = fields
-
-        for k, v in fields.items():
-            setattr(self, k, v)
-
-    def from_fields(self, fields: Dict[str, ModelField]) -> 'SectionSchema':
-        for field_name, field in fields.items():
-            extra = field.field_info.extra
-
-            field_schema = extra.get(
-                FieldSchema.FIELD_SCHEMA, None)
-
-            typeconv = extra.get(FieldSchema.TYPECONV, None)
-            field_schema = field_schema or \
-                self.fieldschema_factory(
-                    required=field.required,
-                    name=field.field_info.title or field.name,
-                    typeclass=field.type_,
-                    typeconv=typeconv,
-                    help=field.field_info.description or '',
-                    default=field.get_default()
-                )
-
-            self.fields[field_name] = field_schema
-            setattr(self, field_name, field_schema)
-
-        return self
+from pydantic import BaseModel
 
 class Section(BaseModel):
     NAME: ClassVar[str] = "default"
@@ -61,18 +8,5 @@ class Section(BaseModel):
     class Config:
         validate_assignment = True
 
-    @classmethod
-    def section_schema(cls) -> SectionSchema:
-        instance = SectionSchema(FieldSchema)
-        return SectionSchema.from_fields(instance, cls.__fields__)
-
 class Config(BaseModel):
-
-    def __init_subclass__(cls) -> None:
-        restrict_field_types(cls, [Section])
-
-    @property
-    def sections(self) -> Dict[str, Type[Section]]:
-        return {
-            k: getattr(self, k) for k in self.__fields_set__
-        }
+    pass
