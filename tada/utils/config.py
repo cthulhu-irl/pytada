3eef3d36c59cbb512ced17f2d7a5231346125ee7
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-08 16:35:34 +0330

    skeleton: add TOMLConfigParser as a parser wrapper for Config for converting toml string to/from Config

--- tada/utils/config.py
+++ tada/utils/config.py
@@ -1,4 +1,17 @@
+from typing import Type
+
 from pydantic import BaseModel
+
+class TOMLConfigParser(object):
+
+    def __init__(self, config_factory: Type['Config']):
+        pass
+
+    def parse(self, string: str) -> 'Config':
+        pass
+
+    def unparse(self, config: 'Config') -> str:
+        pass
 
 class Section(BaseModel):
     class Config:
