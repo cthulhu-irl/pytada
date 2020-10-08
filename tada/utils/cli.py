976714bd2d60b21dec27fbc861d930f0fcb07757
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-08 16:34:29 +0330

    skeleton: add ARGVCLIParser as a parser wrapper for CLI class for argument parsing

--- tada/utils/cli.py
+++ tada/utils/cli.py
@@ -1,4 +1,17 @@
+from typing import Type, List
+
 from pydantic import BaseModel
+
+class ARGVCLIParser(object):
+
+    def __init__(self, cli_factory: Type['CLI'], prog: str = ''):
+        pass
+
+    def parse(self, argv: List[str]) -> 'CLI':
+        pass
+
+    def unparse(self, cli: 'CLI') -> List[str]:
+        pass
 
 class Command(BaseModel):
     pass
