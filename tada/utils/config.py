bfd20ff7445ad10607575899d775b2d6beda3ab3
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-10 23:07:34 +0330

    feat: add tada.utils.config.TOMLConfigParser class and implement

--- tada/utils/config.py
+++ tada/utils/config.py
@@ -1,16 +1,17 @@
 from typing import Type
 
+import toml
 from pydantic import BaseModel
 
 class TOMLConfigParser(object):
 
     @staticmethod
     def parse(klass: Type['Config'], string: str) -> 'Config':
-        pass
+        return klass(**toml.loads(string))
 
     @staticmethod
     def unparse(config: 'Config') -> str:
-        pass
+        return toml.dumps(config.dict())
 
 class Section(BaseModel):
 
