3322add399499a79ad3621c8d1ec01e809dfc2c1
cthulhu-irl <xh4x0r3r@gmail.com>
2020-10-23 01:21:10 +0330

    test: add skeleton/empty test cases for tada.utils.cli.ARGVCLIParser

---
+++ tests/test_utils_argvcliparser.py
@@ -0,0 +1,16 @@
+from tada.utils.cli import ARGVCLIParser, CLI, Command
+
+def test_option_boolean():
+    pass
+
+def test_option_value_conversion():
+    pass
+
+def test_optional_flag_list():
+    pass
+
+def test_optional_positional_list():
+    pass
+
+def test_command_nesting():
+    pass
